35794f4fd9ea54e4a77948e67feb08fb5af06959
Andrew Chow <achow101-github@achow101.com>
2018-03-05 16:45:47 -0500

    Add coin selection test cases and move the original test cases
    Adds test cases for BnB coin selection in a new file, coinselector_tests.cpp.
    Also moves the original coin selection test cases to that file.
    
    Makes bench/coin_selection.cpp work with the new SelectCoinsMinConf

--- src/bench/coin_selection.cpp
+++ src/bench/coin_selection.cpp
@@ -44,7 +44,9 @@ static void CoinSelection(benchmark::State& state)
 
         std::set<CInputCoin> setCoinsRet;
         CAmount nValueRet;
-        bool success = wallet.SelectCoinsMinConf(1003 * COIN, 1, 6, 0, vCoins, setCoinsRet, nValueRet);
+        CAmount fee_ret;
+        bool success = wallet.SelectCoinsMinConf(1003 * COIN, 1, 6, 0, vCoins, setCoinsRet, nValueRet, fee_ret, CFeeRate(0), true, 34, 148)
+                       || wallet.SelectCoinsMinConf(1003 * COIN, 1, 6, 0, vCoins, setCoinsRet, nValueRet, fee_ret, CFeeRate(0), false, 0, 0);
         assert(success);
         assert(nValueRet == 1003 * COIN);
         assert(setCoinsRet.size() == 2);
