60bb234f170f0e96ba0a8d63a638d08ffe2ecf0f
Andrew Chow <achow101-github@achow101.com>
2018-03-09 18:12:34 -0500

    squashme! unify usage of curr

--- src/wallet/coinselection.cpp
+++ src/wallet/coinselection.cpp
@@ -52,26 +52,25 @@ static const size_t TOTAL_TRIES = 100000;
 bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_value, const CAmount& cost_of_change, std::set<CInputCoin>& out_set, CAmount& value_ret, CAmount not_input_fees)
 {
     out_set.clear();
-    CAmount value_track = 0;
+    CAmount curr_value = 0;
 
     int depth = 0;
-    std::vector<bool> selection(utxo_pool.size()); // select the utxo at this index
+    std::vector<bool> curr_selection(utxo_pool.size()); // select the utxo at this index
     bool backtrack = false;
     CAmount actual_target = not_input_fees + target_value;
 
-    // Calculate lookahead
-    CAmount lookahead = 0;
+    // Calculate curr_available_value
+    CAmount curr_available_value = 0;
     for (const CInputCoin& utxo : utxo_pool) {
-        lookahead += utxo.effective_value;
+        curr_available_value += utxo.effective_value;
     }
-    if (lookahead < actual_target) {
+    if (curr_available_value < actual_target) {
         return false;
     }
 
     // Sort the utxo_pool
     std::sort(utxo_pool.begin(), utxo_pool.end(), descending);
 
-    // Best solution
     CAmount curr_waste = 0;
     std::vector<bool> best_selection;
     CAmount best_waste = MAX_MONEY;
@@ -79,21 +78,21 @@ bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_va
     // Depth First search loop for choosing the UTXOs
     for (size_t i = 0; i < TOTAL_TRIES; ++i) {
         // Conditions for starting a backtrack
-        if (value_track + lookahead < actual_target ||                // Cannot possibly reach target with the amount remaining in the lookahead.
-            value_track > actual_target + cost_of_change ||    // Selected value is out of range, go back and try other branch
+        if (curr_value + curr_available_value < actual_target ||                // Cannot possibly reach target with the amount remaining in the curr_available_value.
+            curr_value > actual_target + cost_of_change ||    // Selected value is out of range, go back and try other branch
             (curr_waste > best_waste && (utxo_pool.at(0).fee - utxo_pool.at(0).long_term_fee) > 0)) { // Don't select things which we know will be more wasteful if the waste is increasing
             backtrack = true;
-        } else if (value_track >= actual_target) {       // Selected value is within range
-            curr_waste += (value_track - actual_target); // This is the excess value which is added to the waste for the below comparison
+        } else if (curr_value >= actual_target) {       // Selected value is within range
+            curr_waste += (curr_value - actual_target); // This is the excess value which is added to the waste for the below comparison
             // Adding another UTXO after this check could bring the waste down if the long term fee is higher than the current fee.
             // However we are not going to explore that because this optimization for the waste is only done when we have hit our target
             // value. Adding any more UTXOs will be just burning the UTXO; it will go entirely to fees. Thus we aren't going to
             // explore any more UTXOs to avoid burning money like that.
             if (curr_waste <= best_waste) {
-                best_selection.assign(selection.begin(), selection.end());
+                best_selection.assign(curr_selection.begin(), curr_selection.end());
                 best_waste = curr_waste;
             }
-            curr_waste -= (value_track - actual_target); // Remove the excess value as we will be selecting different coins now
+            curr_waste -= (curr_value - actual_target); // Remove the excess value as we will be selecting different coins now
             backtrack = true;
         }
 
@@ -103,9 +102,9 @@ bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_va
             --depth;
 
             // Walk backwards to find the last included UTXO that still needs to have its omission branch traversed.
-            while (depth >= 0 && !selection.at(depth)) {
+            while (depth >= 0 && !curr_selection.at(depth)) {
                 // Step back one
-                lookahead += utxo_pool.at(depth).effective_value;
+                curr_available_value += utxo_pool.at(depth).effective_value;
                 --depth;
             }
             if (depth < 0) { // We have walked back to the first utxo and no branch is untraversed. All solutions searched
@@ -113,8 +112,8 @@ bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_va
             }
 
             // These were always included first, try excluding now
-            selection.at(depth) = false;
-            value_track -= utxo_pool.at(depth).effective_value;
+            curr_selection.at(depth) = false;
+            curr_value -= utxo_pool.at(depth).effective_value;
             curr_waste -= (utxo_pool.at(depth).fee - utxo_pool.at(depth).long_term_fee);
             ++depth;
         } else { // Moving forwards, continuing down this branch
@@ -123,20 +122,20 @@ bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_va
 
             // Avoid searching a branch if the previous UTXO has the same value and same waste and was excluded. Since the ratio of fee to
             // long term fee is the same, we only need to check if one of those values match in order to know that the waste is the same.
-            if (depth > 0 && !selection.at(depth - 1) &&
+            if (depth > 0 && !curr_selection.at(depth - 1) &&
                 utxo_pool.at(depth).effective_value == utxo_pool.at(depth - 1).effective_value &&
                 utxo_pool.at(depth).fee == utxo_pool.at(depth - 1).fee) {
-                selection.at(depth) = false;
-                lookahead -= utxo_pool.at(depth).effective_value;
+                curr_selection.at(depth) = false;
+                curr_available_value -= utxo_pool.at(depth).effective_value;
                 ++depth;
             } else {
-                // Remove this utxo from the lookahead utxo amount
-                lookahead -= utxo_pool.at(depth).effective_value;
+                // Remove this utxo from the curr_available_value utxo amount
+                curr_available_value -= utxo_pool.at(depth).effective_value;
                 // Increase waste
                 curr_waste += (utxo_pool.at(depth).fee - utxo_pool.at(depth).long_term_fee);
                 // Inclusion branch first (Largest First Exploration)
-                selection.at(depth) = true;
-                value_track += utxo_pool.at(depth).effective_value;
+                curr_selection.at(depth) = true;
+                curr_value += utxo_pool.at(depth).effective_value;
                 ++depth;
             }
         }
