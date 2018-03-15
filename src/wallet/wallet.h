df48ccc7861c4da274a87f5eac8386486ed7daa7
Andrew Chow <achow101-github@achow101.com>
2018-03-15 01:37:36 -0400

    Have SelectCoinsMinConf figure out when to use BnB or SRD
    Instead of the caller telling SelectCoinsMinConf when to use BnB
    or the fallback, have SelectCoinsMinConf do it itself. This removes
    the use_bnb field of coin_selection_params

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -647,13 +647,12 @@ private:
 
 struct CoinSelectionParams
 {
-    bool use_bnb = true;
     size_t change_output_size = 0;
     size_t change_spend_size = 0;
     CFeeRate effective_fee = CFeeRate(0);
     size_t tx_noinputs_size = 0;
 
-    CoinSelectionParams(bool use_bnb, size_t change_output_size, size_t change_spend_size, CFeeRate effective_fee, size_t tx_noinputs_size) : use_bnb(use_bnb), change_output_size(change_output_size), change_spend_size(change_spend_size), effective_fee(effective_fee), tx_noinputs_size(tx_noinputs_size) {}
+    CoinSelectionParams(size_t change_output_size, size_t change_spend_size, CFeeRate effective_fee, size_t tx_noinputs_size) : change_output_size(change_output_size), change_spend_size(change_spend_size), effective_fee(effective_fee), tx_noinputs_size(tx_noinputs_size) {}
     CoinSelectionParams() {}
 };
 
