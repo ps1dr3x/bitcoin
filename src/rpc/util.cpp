e7dcff0a571866a9487111dc087b4a6e0e9ad5b6
Andrew Chow <achow101-github@achow101.com>
2018-10-04 23:22:03 -0400

    Create RPCHelpUniValue
    RPCHelpUniValue is a subclass of UniValue which holds help information
    in addition to the Univalue values. it is to be used with creating the
    help text commands.
    
    RPCHelpUniValue will help make UniValue arguments and results have
    consistent spacing.

--- src/rpc/util.cpp
+++ src/rpc/util.cpp
@@ -126,3 +126,228 @@ UniValue DescribeAddress(const CTxDestination& dest)
 {
     return boost::apply_visitor(DescribeAddressVisitor(), dest);
 }
+
+bool RPCHelpUniValue::findKey(const std::string& key, size_t& retIdx) const
+{
+    for (size_t i = 0; i < keys.size(); i++) {
+        if (keys[i] == key) {
+            retIdx = i;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool RPCHelpUniValue::push_back(const RPCHelpUniValue& val_)
+{
+    if (typ != VARR)
+        return false;
+
+    values.push_back(val_);
+    return true;
+}
+
+bool RPCHelpUniValue::push_backV(const std::vector<RPCHelpUniValue>& vec)
+{
+    if (typ != VARR)
+        return false;
+
+    values.insert(values.end(), vec.begin(), vec.end());
+
+    return true;
+}
+
+void RPCHelpUniValue::__pushKV(const std::string& key, const RPCHelpUniValue& val_, const std::string& m_help, bool m_required)
+{
+    keys.push_back(key);
+    values.push_back(val_);
+    helps.push_back(m_help);
+    requireds.push_back(m_required);
+}
+
+bool RPCHelpUniValue::pushKV(const std::string& key, const RPCHelpUniValue& val_, const std::string& m_help, bool m_required)
+{
+    if (typ != VOBJ)
+        return false;
+
+    size_t idx;
+    if (findKey(key, idx)) {
+        values[idx] = val_;
+        helps[idx] = m_help;
+        requireds[idx] = m_required;
+    }
+    else
+        __pushKV(key, val_, m_help, m_required);
+    return true;
+}
+
+bool RPCHelpUniValue::pushKVs(const RPCHelpUniValue& obj)
+{
+    if (typ != VOBJ || obj.typ != VOBJ)
+        return false;
+
+    for (size_t i = 0; i < obj.keys.size(); i++)
+        __pushKV(obj.keys[i], obj.values.at(i), obj.helps[i], obj.requireds[i]);
+
+    return true;
+}
+
+std::string RPCHelpUniValue::WriteUniv(unsigned int prettyIndent, unsigned int indentLevel) const
+{
+    return UniValue::write(prettyIndent, indentLevel);
+}
+
+std::string RPCHelpUniValue::write(unsigned int pretty_indent, unsigned int indent_level) const
+{
+    std::vector<std::string> left;
+    GetLeft(pretty_indent, indent_level, left);
+    std::vector<std::string> right;
+    GetRight(right);
+    assert(left.size() == right.size());
+
+    // Find longest left string
+    size_t n_spaces = 0;
+    for (const std::string& str : left) {
+        if (str.size() > n_spaces) {
+            n_spaces = str.size();
+        }
+    }
+    n_spaces += 4;
+
+    // Join everything together with correct spacing
+    std::string s;
+    s.reserve(1024);
+    for (size_t i = 0; i < left.size(); ++i) {
+        s += left[i];
+        s.append(n_spaces - left[i].size() , ' ');
+        s += right[i];
+        s += "\n";
+    }
+
+    return s;
+}
+
+static void IndentStr(unsigned int pretty_indent, unsigned int indent_level, std::string& s)
+{
+    s.append(pretty_indent * indent_level, ' ');
+}
+
+void RPCHelpUniValue::GetLeft(unsigned int pretty_indent, unsigned int indent_level, std::vector<std::string>& out) const
+{
+    unsigned int mod_indent = indent_level;
+    if (mod_indent == 0)
+        mod_indent = 1;
+
+    switch (typ) {
+    case VOBJ:
+        GetObjectLeft(pretty_indent, mod_indent, out);
+        break;
+    case VARR:
+        GetArrayLeft(pretty_indent, mod_indent, out);
+        break;
+    default:
+        out.push_back(WriteUniv(pretty_indent, indent_level));
+        break;
+    }
+}
+
+void RPCHelpUniValue::GetRight(std::vector<std::string>& out) const
+{
+    switch (typ) {
+    case VOBJ:
+        GetObjectRight(out);
+        break;
+    case VARR:
+        GetArrayRight(out);
+        break;
+    default:
+        out.push_back(std::string("(") + uvTypeName(typ) + (required ? " required) " : " optional) ") + help);
+        break;
+    }
+}
+
+void RPCHelpUniValue::GetArrayLeft(unsigned int pretty_indent, unsigned int indent_level, std::vector<std::string>& out) const
+{
+    std::string open_brace;
+    IndentStr(pretty_indent, indent_level, open_brace);
+    open_brace += "[";
+    out.push_back(open_brace);
+
+    for (unsigned int i = 0; i < values.size(); i++) {
+        std::vector<std::string> arr_vals;
+        values[i].GetLeft(pretty_indent, 0, arr_vals);
+        for (const std::string& arr_val : arr_vals) {
+            std::string indent;
+            IndentStr(pretty_indent, indent_level + 1, indent);
+            out.push_back(indent + arr_val);
+        }
+        std::string ellipsis;
+        IndentStr(pretty_indent, indent_level + 1, ellipsis);
+        out.push_back(ellipsis + ",...");
+    }
+
+    std::string close_brace;
+    IndentStr(pretty_indent, indent_level, close_brace);
+    close_brace += "]";
+    out.push_back(close_brace);
+}
+
+void RPCHelpUniValue::GetArrayRight(std::vector<std::string>& out) const
+{
+    if (!help.empty()) {
+        out.push_back(std::string("(array, ") + (required ? "required" : "optional") + ") " + help);
+    } else {
+        out.push_back("");
+    }
+
+    for (unsigned int i = 0; i < values.size(); i++) {
+        values[i].GetRight(out);
+        out.push_back("");
+    }
+
+    out.push_back("");
+}
+
+void RPCHelpUniValue::GetObjectLeft(unsigned int pretty_indent, unsigned int indent_level, std::vector<std::string>& out) const
+{
+    std::string open_brace;
+    IndentStr(pretty_indent, indent_level, open_brace);
+    open_brace += "{";
+    out.push_back(open_brace);
+
+    for (unsigned int i = 0; i < keys.size(); i++) {
+        std::string s;
+        IndentStr(pretty_indent, indent_level, s);
+        s += "\"" + keys[i] + "\":";
+        if (values.at(i).typ != VARR && values.at(i).typ != VOBJ) {
+            s += " " + values.at(i).WriteUniv(pretty_indent, indent_level + 1);
+            out.push_back(s);
+        } else {
+            out.push_back(s);
+            values.at(i).GetLeft(pretty_indent, indent_level + 1, out);
+        }
+        if (i != (values.size() - 1))
+            out.back() += ",";
+    }
+
+    std::string close_brace;
+    IndentStr(pretty_indent, indent_level, close_brace);
+    close_brace += "}";
+    out.push_back(close_brace);
+}
+
+void RPCHelpUniValue::GetObjectRight(std::vector<std::string>& out) const
+{
+    if (!help.empty()) {
+        out.push_back(std::string("(object, ") + (required ? "required" : "optional") + ") " + help);
+    } else {
+        out.push_back("");
+    }
+
+    for (unsigned int i = 0; i < keys.size(); i++) {
+        values.at(i).GetRight(out);
+    }
+
+    out.push_back("");
+}
--- src/rpc/util.h
+++ src/rpc/util.h
@@ -20,8 +20,139 @@ class CScript;
 
 CPubKey HexToPubKey(const std::string& hex_in);
 CPubKey AddrToPubKey(CKeyStore* const keystore, const std::string& addr_in);
-CScript CreateMultisigRedeemscript(const int required, const std::vector<CPubKey>& pubkeys);
+CScript CreateMultisigRedeemscript(const int m_required, const std::vector<CPubKey>& pubkeys);
 
 UniValue DescribeAddress(const CTxDestination& dest);
+
+class RPCHelpUniValue : public UniValue
+{
+public:
+    RPCHelpUniValue(UniValue::VType initialType, const std::string& initialStr = "", const std::string& m_help = "", bool m_required = false) {
+        typ = initialType;
+        val = initialStr;
+        help = m_help;
+        required = m_required;
+    }
+    RPCHelpUniValue(uint64_t val_, const std::string& m_help = "", bool m_required = false) {
+        setInt(val_);
+        help = m_help;
+        required = m_required;
+    }
+    RPCHelpUniValue(int64_t val_, const std::string& m_help = "", bool m_required = false) {
+        setInt(val_);
+        help = m_help;
+        required = m_required;
+    }
+    RPCHelpUniValue(bool val_, const std::string& m_help = "", bool m_required = false) {
+        setBool(val_);
+        help = m_help;
+        required = m_required;
+    }
+    RPCHelpUniValue(int val_, const std::string& m_help = "", bool m_required = false) {
+        setInt(val_);
+        help = m_help;
+        required = m_required;
+    }
+    RPCHelpUniValue(double val_, const std::string& m_help = "", bool m_required = false) {
+        setFloat(val_);
+        help = m_help;
+        required = m_required;
+    }
+    RPCHelpUniValue(const std::string& val_, const std::string& m_help = "", bool m_required = false) {
+        setStr(val_);
+        help = m_help;
+        required = m_required;
+    }
+    RPCHelpUniValue(const char *val_, const std::string& m_help = "", bool m_required = false) {
+        std::string s(val_);
+        setStr(s);
+        help = m_help;
+        required = m_required;
+    }
+    ~RPCHelpUniValue() {}
+
+    void clear();
+
+
+    bool push_back(const RPCHelpUniValue& val);
+    bool push_back(const std::string& val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal(VSTR, val_, m_help, m_required);
+        return push_back(tmpVal);
+    }
+    bool push_back(const char *val_, const std::string& m_help = "", bool m_required = false) {
+        std::string s(val_);
+        return push_back(s);
+    }
+    bool push_back(uint64_t val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal(val_, m_help, m_required);
+        return push_back(tmpVal);
+    }
+    bool push_back(int64_t val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal(val_, m_help, m_required);
+        return push_back(tmpVal);
+    }
+    bool push_back(int val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal(val_, m_help, m_required);
+        return push_back(tmpVal);
+    }
+    bool push_back(double val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal(val_, m_help, m_required);
+        return push_back(tmpVal);
+    }
+    bool push_backV(const std::vector<RPCHelpUniValue>& vec);
+
+    void __pushKV(const std::string& key, const RPCHelpUniValue& val, const std::string& m_help = "", bool m_required = false);
+    bool pushKV(const std::string& key, const RPCHelpUniValue& val, const std::string& m_help = "", bool m_required = false);
+    bool pushKV(const std::string& key, const std::string& val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal(VSTR, val_, m_help, m_required);
+        return pushKV(key, tmpVal);
+    }
+    bool pushKV(const std::string& key, const char *val_, const std::string& m_help = "", bool m_required = false) {
+        std::string _val(val_);
+        return pushKV(key, _val, m_help, m_required);
+    }
+    bool pushKV(const std::string& key, int64_t val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal(val_, m_help, m_required);
+        return pushKV(key, tmpVal);
+    }
+    bool pushKV(const std::string& key, uint64_t val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal(val_, m_help, m_required);
+        return pushKV(key, tmpVal);
+    }
+    bool pushKV(const std::string& key, bool val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal((bool)val_, m_help, m_required);
+        return pushKV(key, tmpVal);
+    }
+    bool pushKV(const std::string& key, int val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal((int64_t)val_, m_help, m_required);
+        return pushKV(key, tmpVal);
+    }
+    bool pushKV(const std::string& key, double val_, const std::string& m_help = "", bool m_required = false) {
+        RPCHelpUniValue tmpVal(val_, m_help, m_required);
+        return pushKV(key, tmpVal);
+    }
+    bool pushKVs(const RPCHelpUniValue& obj);
+
+    std::string write(unsigned int prettyIndent = 0, unsigned int indentLevel = 0) const;
+    std::string WriteUniv(unsigned int prettyIndent = 0, unsigned int indentLevel = 0) const;
+
+private:
+    UniValue::VType typ;
+    std::string val;                       // numbers are stored as C++ strings
+    std::vector<std::string> keys;
+    std::string help;
+    std::vector<std::string> helps;
+    bool required;
+    std::vector<bool> requireds;
+    std::vector<RPCHelpUniValue> values;
+
+    bool findKey(const std::string& key, size_t& retIdx) const;
+    void GetLeft(unsigned int pretty_indent, unsigned int indent_level, std::vector<std::string>& out) const;
+    void GetRight(std::vector<std::string>& out) const;
+    void GetArrayLeft(unsigned int pretty_indent, unsigned int indent_level, std::vector<std::string>& out) const;
+    void GetArrayRight(std::vector<std::string>& out) const;
+    void GetObjectLeft(unsigned int pretty_indent, unsigned int indent_level, std::vector<std::string>& out) const;
+    void GetObjectRight(std::vector<std::string>& out) const;
+};
 
 #endif // BITCOIN_RPC_UTIL_H
